f77a45947a7ff2417969e8f9cde9f8a9fac23b66
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-13 14:49:01 +0200

    gh-add: messages.html

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render,redirect
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login,logout,authenticate
+from urllib.parse import urlparse, parse_qs
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 # Create your views here.
 class HomePage(View):
     def get(self,request):
         return render(request,'index.html')
+
+
+class Login(View):
+    def get(self,request):
+        return render(request,'login.html')
+    
+    def post(self,request):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request,'User not found!')
+                return render(request, 'login.html')
