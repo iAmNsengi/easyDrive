229f11d48cf3864530a6fac241db5c58b7d499cb
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-13 16:15:09 +0200

    feat: created models driver,job,company

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Driver(models.Model):
+    user = models.ForeignKey(User, on_delete=User)
+    avatar = models.ImageField(upload_to='driver/',null="True")
+    category = models.CharField(max_length=100)
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.user.username + " " + self.category
+
+
+class Company(models.Model):
+    user = models.ForeignKey(User, on_delete=User)
+    avatar = models.ImageField(upload_to='company/')
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.user.username + " " + self.category
+
+
+class Job(models.Model):
+    creator = models.ForeignKey(User,on_delete=models.CASCADE)
+    applicants = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.creator.username+" "+str(self.created_at)
+
