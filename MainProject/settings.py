362d3354c4aaeca0dbe07ae226a411ff768853e9
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-14 20:55:24 +0200

    feat: displaying time

--- MainProject/settings.py
+++ MainProject/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "MainApp"
+    "MainApp",
+    "django.contrib.humanize"
 ]
 
 MIDDLEWARE = [
