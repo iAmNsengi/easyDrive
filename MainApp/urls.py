87ceb5c2197a0ddfa89c399aae27ec11a72ff49a
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-14 19:14:15 +0200

    fix: update urls.py for url re-routing

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns =[
     path('',HomePage.as_view()),
-    path('jobs',Jobs.as_view()),
-    path('login',Login.as_view()),
-    path('register',Register.as_view()),
-    path('logout',Logout)
+    path('jobs/',Jobs.as_view()),
+    path('login/',Login.as_view()),
+    path('register/',Register.as_view()),
+    path('logout/',Logout)
 
 ]
